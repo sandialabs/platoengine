1dc3c3152394408b97de423f28590bd448bcd375
Ray Wildman <rawildm@sandia.gov>
2024-08-06 07:44:42 -0600

    Fixes test fixture for parallel tests

--- functional/plato/test_utilities/FileCreatingTestFixture.cpp
+++ functional/plato/test_utilities/FileCreatingTestFixture.cpp
@@ -1,16 +1,36 @@
 #include "plato/test_utilities/FileCreatingTestFixture.hpp"
 
+#include <boost/mpi.hpp>
 #include <fstream>
 
 namespace plato::test_utilities
 {
-FileCreatingTestFixture::FileCreatingTestFixture(std::filesystem::path aFilePath) : mFilePath{std::move(aFilePath)}
+template <typename F, typename... Args>
+void execute_on_root(const F& aFunction, Args&&... aArgs)
 {
-    auto tStream = std::ofstream{mFilePath};
-    tStream.close();
+    const auto tComm = boost::mpi::communicator{};
+    if (tComm.rank() == 0)
+    {
+        aFunction(std::forward<Args>(aArgs)...);
+    }
+    tComm.barrier();
 }
 
-FileCreatingTestFixture::~FileCreatingTestFixture() { std::filesystem::remove(mFilePath); }
+FileCreatingTestFixture::FileCreatingTestFixture(std::filesystem::path aFilePath) : mFilePath{std::move(aFilePath)}
+{
+    execute_on_root(
+        [](const std::filesystem::path& aFilePath)
+        {
+            auto tStream = std::ofstream{aFilePath};
+            tStream.close();
+        },
+        mFilePath);
+}
+
+FileCreatingTestFixture::~FileCreatingTestFixture()
+{
+    execute_on_root([](const std::filesystem::path& aFilePath) { std::filesystem::remove(aFilePath); }, mFilePath);
+}
 
 const std::filesystem::path& FileCreatingTestFixture::filePath() const { return mFilePath; }
 
