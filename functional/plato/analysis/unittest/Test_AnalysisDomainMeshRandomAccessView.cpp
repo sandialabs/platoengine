d5830e5062ccd344efbf558d86b1a9db3428e46d
Ray Wildman <rawildm@sandia.gov>
2024-11-27 08:43:09 -0700

    Fixes tests to check for non-existent entries in a mutable view

--- functional/plato/analysis/unittest/Test_AnalysisDomainMeshRandomAccessView.cpp
+++ functional/plato/analysis/unittest/Test_AnalysisDomainMeshRandomAccessView.cpp
@@ -66,6 +66,17 @@ void check_nonexistent_entries(const AnalysisDomainMeshRandomAccessView aAnalysi
         EXPECT_FALSE(aAnalysisDomainMeshView[tID]) << aTestContext;
     }
 }
+
+void check_nonexistent_entries(const AnalysisDomainMeshMutableRandomAccessView aAnalysisDomainMeshView,
+                               const std::vector<std::size_t>& aIDs,
+                               const test_utilities::TestContext& aTestContext)
+{
+    for (const auto tID : aIDs)
+    {
+        EXPECT_TRUE(aAnalysisDomainMeshView[tID].mIterators.empty()) << aTestContext;
+    }
+}
+
 }  // namespace
 
 TEST(AnalysisDomainMeshRandomAccessView, OneBlockSize)
@@ -84,10 +95,10 @@ TEST(AnalysisDomainMeshRandomAccessView, OneBlockAccess)
 {
     const auto tBlockScalarField = AnalysisDomainMesh::BlockScalarField{{1, scalar_field_vector(kIDs1, kScalarField1)}};
     auto tAnalysisDomainMesh = AnalysisDomainMesh{kFileName, tBlockScalarField};
-    const auto tRandomAccessView = AnalysisDomainMeshRandomAccessView{tAnalysisDomainMesh};
 
     // Const view
     {
+        const auto tRandomAccessView = AnalysisDomainMeshRandomAccessView{tAnalysisDomainMesh};
         check_view_vs_vector(tRandomAccessView, kIDs1, kScalarField1, TEST_CONTEXT("Field 1"));
         check_nonexistent_entries(tRandomAccessView, kNonExistentIDs, TEST_CONTEXT("Non-existent entries"));
     }
@@ -102,7 +113,7 @@ TEST(AnalysisDomainMeshRandomAccessView, OneBlockAccess)
 
         check_vectors(tAnalysisDomainMesh.mBlockScalarField.at(1), tExpectedFieldValues,
                       TEST_CONTEXT("Modified field 1"));
-        check_nonexistent_entries(tRandomAccessView, kNonExistentIDs,
+        check_nonexistent_entries(tRandomAccessMutableView, kNonExistentIDs,
                                   TEST_CONTEXT("Non-existent entries in mutable view"));
     }
 }
@@ -112,10 +123,10 @@ TEST(AnalysisDomainMeshRandomAccessView, TwoBlockAccessNoOverlap)
     const auto tBlockScalarField = AnalysisDomainMesh::BlockScalarField{{1, scalar_field_vector(kIDs1, kScalarField1)},
                                                                         {3, scalar_field_vector(kIDs3, kScalarField3)}};
     auto tAnalysisDomainMesh = AnalysisDomainMesh{kFileName, tBlockScalarField};
-    const auto tRandomAccessView = AnalysisDomainMeshRandomAccessView{tAnalysisDomainMesh};
 
     // Const view
     {
+        const auto tRandomAccessView = AnalysisDomainMeshRandomAccessView{tAnalysisDomainMesh};
         check_view_vs_vector(tRandomAccessView, kIDs1, kScalarField1, TEST_CONTEXT("Field 1"));
         check_view_vs_vector(tRandomAccessView, kIDs3, kScalarField3, TEST_CONTEXT("Field 3"));
         check_nonexistent_entries(tRandomAccessView, kNonExistentIDs, TEST_CONTEXT("Non-existent entries"));
@@ -136,7 +147,7 @@ TEST(AnalysisDomainMeshRandomAccessView, TwoBlockAccessNoOverlap)
         tExpectedFieldValues3.front() = tNewValue;
         check_vectors(tAnalysisDomainMesh.mBlockScalarField.at(3), tExpectedFieldValues3,
                       TEST_CONTEXT("Modified field 3"));
-        check_nonexistent_entries(tRandomAccessView, kNonExistentIDs,
+        check_nonexistent_entries(tRandomAccessMutableView, kNonExistentIDs,
                                   TEST_CONTEXT("Non-existent entries in mutable view"));
     }
 }
@@ -146,10 +157,10 @@ TEST(AnalysisDomainMeshRandomAccessView, TwoBlockAccessHasOverlap)
     const auto tBlockScalarField = AnalysisDomainMesh::BlockScalarField{{1, scalar_field_vector(kIDs1, kScalarField1)},
                                                                         {2, scalar_field_vector(kIDs2, kScalarField2)}};
     auto tAnalysisDomainMesh = AnalysisDomainMesh{kFileName, tBlockScalarField};
-    const auto tRandomAccessView = AnalysisDomainMeshRandomAccessView{tAnalysisDomainMesh};
 
     // Const view
     {
+        const auto tRandomAccessView = AnalysisDomainMeshRandomAccessView{tAnalysisDomainMesh};
         check_view_vs_vector(tRandomAccessView, kIDs1, kScalarField1, TEST_CONTEXT("Field 1"));
         check_view_vs_vector(tRandomAccessView, kIDs2, kScalarField2, TEST_CONTEXT("Field 2"));
         check_nonexistent_entries(tRandomAccessView, kNonExistentIDs, TEST_CONTEXT("Non-existent entries"));
@@ -171,7 +182,7 @@ TEST(AnalysisDomainMeshRandomAccessView, TwoBlockAccessHasOverlap)
         tExpectedFieldValues2.front() = tNewValue;
         check_vectors(tAnalysisDomainMesh.mBlockScalarField.at(2), tExpectedFieldValues2,
                       TEST_CONTEXT("Modified field 2"));
-        check_nonexistent_entries(tRandomAccessView, kNonExistentIDs,
+        check_nonexistent_entries(tRandomAccessMutableView, kNonExistentIDs,
                                   TEST_CONTEXT("Non-existent entries in mutable view"));
     }
 }
