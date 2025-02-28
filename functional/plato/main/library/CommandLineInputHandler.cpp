eddbd136a016e6da75bcb17ea1be733762749f86
Ray Wildman <rawildm@sandia.gov>
2025-02-28 15:53:00 -0700

    Adds warining for missing process managers

--- functional/plato/main/library/CommandLineInputHandler.cpp
+++ functional/plato/main/library/CommandLineInputHandler.cpp
@@ -9,15 +9,31 @@
 #include "plato/main/library/Executor.hpp"
 #include "plato/process_manager/library/ProcessManagerData.hpp"
 #include "plato/process_manager/library/ProcessManagerFactory.hpp"
+#include "plato/process_manager/library/ProcessManagerRegistration.hpp"
 #include "plato/process_manager/library/ValidatedInput.hpp"
 #include "plato/utilities/Exception.hpp"
+#include "plato/utilities/StringUtilities.hpp"
 
 namespace plato::main::library
 {
 namespace
 {
 constexpr auto kHelpKey = std::string_view{"--help"};
+
+void unregistered_process_manager_warning(
+    const process_manager::library::ValidatedProcessManagerInputVector& aValidatedProcessManagerInput)
+{
+    const auto tUnregisteredProcessManagers =
+        process_manager::library::unregistered_process_managers(aValidatedProcessManagerInput);
+    if (!tUnregisteredProcessManagers.empty())
+    {
+        std::cout << "Warning: The following process managers are in the input, but are not implemented in this build "
+                     "of plato:\n";
+        std::cout << utilities::concatenate_container(tUnregisteredProcessManagers, "\n");
+        std::cout << "\nThese process managers will be skipped.\n";
+    }
 }
+}  // namespace
 
 void handle_input(const std::vector<std::string>& aArguments)
 {
@@ -44,6 +60,7 @@ void run_plato(const std::filesystem::path& aInputFile)
     {
         namespace ppml = plato::process_manager::library;
         const auto tValidatedInput = ppml::parse_and_validate_from_file(aInputFile);
+        unregistered_process_manager_warning(tValidatedInput.processManagers());
         const auto tExecutor =
             plato::main::library::Executor{ppml::make_process_managers(tValidatedInput.processManagers())};
         const auto tProcessManagerData = ppml::make_process_manager_data(tValidatedInput);
