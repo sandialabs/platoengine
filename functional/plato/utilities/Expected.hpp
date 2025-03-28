a3de9b8a34a24ac0d250d22df076193d27ea2653
Ray Wildman <rawildm@sandia.gov>
2025-03-28 15:20:57 -0600

    Adds more documentation

--- functional/plato/utilities/Expected.hpp
+++ functional/plato/utilities/Expected.hpp
@@ -8,6 +8,10 @@
 
 namespace plato::utilities
 {
+/// @brief A wrapper for an unexpected error type.
+///
+/// This may be used for assigning an error type to an expected object, though prefer the free function unexpected to
+/// deduce the type.
 template <typename Error>
 struct Unexpected
 {
@@ -15,6 +19,13 @@ struct Unexpected
 };
 
 /// @brief Helper function for deducing the error type.
+///
+/// This may be used for assigning an error type to an expected object. The assignment is converting, so the following
+/// will work:
+/// @code{.cpp}
+/// auto tExpected = Expected<int, std::string>{42};
+//  tExpected = unexpected("Error!");
+/// @endcode
 template <typename Error>
 constexpr auto unexpected(Error&& aError) -> Unexpected<Error>;
 
@@ -26,7 +37,9 @@ constexpr bool kIsUnexpected<Unexpected<U>> = true;
 
 /// @brief An minimal implementation of std::expected until we can use c++23
 ///
-/// See https://en.cppreference.com/w/cpp/utility/expected
+/// The main use of this is to express an error condition using optional-like semantics. The main differences between
+/// this type and `optional` is that this has an error member function that will return an error object. See also
+/// https://en.cppreference.com/w/cpp/utility/expected
 template <typename T, typename Error>
 class Expected
 {
