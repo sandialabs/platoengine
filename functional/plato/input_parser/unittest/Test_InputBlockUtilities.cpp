5db2ee50198b9c731880304508225447835bf5f0
Ray Wildman <rawildm@sandia.gov>
2025-04-04 15:57:12 -0600

    Adds tests of all input blocks with pipe operator

--- functional/plato/input_parser/unittest/Test_InputBlockUtilities.cpp
+++ functional/plato/input_parser/unittest/Test_InputBlockUtilities.cpp
@@ -5,6 +5,7 @@
 #include "plato/input_parser/InputBlockStruct.hpp"
 #include "plato/input_parser/InputBlockUtilities.hpp"
 #include "plato/input_parser/ParsedInput.hpp"
+#include "plato/test_utilities/TestContext.hpp"
 
 // clang-format off
 PLATO_GEOMETRY_INPUT_BLOCK_STRUCT(
@@ -15,31 +16,72 @@ PLATO_PROCESS_MANAGER_INPUT_BLOCK_STRUCT(
     (plato)(input_parser), TestProcessManagerBlock, 
     (int, field1, "help")
 )
+PLATO_FILTER_INPUT_BLOCK_STRUCT(
+    (plato)(input_parser), TestFilterBlock, 
+    (int, field1, "help")
+)
+PLATO_NAMED_INPUT_BLOCK_STRUCT(
+    (plato)(input_parser), TestObjectiveBlock,
+    plato::input_parser::ComponentType::kObjective,
+    (int, field1, "help")
+)
+PLATO_NAMED_INPUT_BLOCK_STRUCT(
+    (plato)(input_parser), TestConstraintBlock,
+    plato::input_parser::ComponentType::kConstraint,
+    (int, field1, "help")
+)
 // clang-format on
 
 namespace plato::input_parser::unittest
 {
+namespace
+{
+template <ComponentType kComponentType, typename InputBlock>
+void check_input_block(const NewParsedInput& aFullInput,
+                       const InputBlock& aExpectedBlock,
+                       const test_utilities::TestContext& aTestContext)
+{
+    const auto& tComponent = aFullInput.get<kComponentType>();
+    ASSERT_EQ(tComponent.size(), 1U) << aTestContext;
+    EXPECT_EQ(tComponent.front().mComponentType, kComponentType) << aTestContext;
+    EXPECT_EQ(tComponent.front().mBlockName, block_name<InputBlock>()) << aTestContext;
+    ASSERT_TRUE(tComponent.front().mInput.template holds_expected_type<InputBlock>()) << aTestContext;
+    EXPECT_EQ(tComponent.front().mInput.template get<InputBlock>().field1, aExpectedBlock.field1) << aTestContext;
+    if constexpr (kIsNamedComponent<kComponentType>)
+    {
+        EXPECT_EQ(tComponent.front().mInput.template get<InputBlock>().name, aExpectedBlock.name) << aTestContext;
+    }
+}
+}  // namespace
+
 TEST(InputBlockUtilities, PipeOperator)
 {
-    const auto tCombinedInput =
-        NewParsedInput{{}} | TestGeometryBlock{/*.field1=*/42} | TestProcessManagerBlock{/*.field1=*/43};
+    const auto tGeometryBlock = TestGeometryBlock{/*.field1=*/42};
+    const auto tProcessManagerBlock = TestProcessManagerBlock{/*.field1=*/43};
+    const auto tCombinedInput = NewParsedInput{{}} | tGeometryBlock | tProcessManagerBlock;
 
-    const auto& tGeometries = tCombinedInput.get<ComponentType::kGeometry>();
-    ASSERT_EQ(tGeometries.size(), 1U);
-    EXPECT_EQ(tGeometries.front().mComponentType, ComponentType::kGeometry);
-    EXPECT_EQ(tGeometries.front().mBlockName, block_name<TestGeometryBlock>());
-    ASSERT_TRUE(tGeometries.front().mInput.holds_expected_type<TestGeometryBlock>());
-    EXPECT_EQ(tGeometries.front().mInput.get<TestGeometryBlock>().field1, 42);
-
-    const auto& tProcessManagers = tCombinedInput.get<ComponentType::kProcessManager>();
-    ASSERT_EQ(tProcessManagers.size(), 1U);
-    EXPECT_EQ(tProcessManagers.front().mComponentType, ComponentType::kProcessManager);
-    EXPECT_EQ(tProcessManagers.front().mBlockName, block_name<TestProcessManagerBlock>());
-    ASSERT_TRUE(tProcessManagers.front().mInput.holds_expected_type<TestProcessManagerBlock>());
-    EXPECT_EQ(tProcessManagers.front().mInput.get<TestProcessManagerBlock>().field1, 43);
+    check_input_block<ComponentType::kGeometry>(tCombinedInput, tGeometryBlock, TEST_CONTEXT("Geometry block"));
+    check_input_block<ComponentType::kProcessManager>(tCombinedInput, tProcessManagerBlock,
+                                                      TEST_CONTEXT("Process manager block"));
 
     EXPECT_TRUE(tCombinedInput.get<ComponentType::kFilter>().empty());
     EXPECT_TRUE(tCombinedInput.get<ComponentType::kObjective>().empty());
     EXPECT_TRUE(tCombinedInput.get<ComponentType::kConstraint>().empty());
 }
+
+TEST(InputBlockUtilities, PipeOperatorNamedTypes)
+{
+    const auto tFilterBlock = TestFilterBlock{/*.field1=*/40};
+    const auto tObjectiveBlock = TestObjectiveBlock{/*.name=*/std::string{"bob"}, /*.field1=*/41};
+    const auto tConstraintBlock = TestConstraintBlock{/*.name=*/std::string{"sally"}, /*.field1=*/42};
+    const auto tCombinedInput = NewParsedInput{{}} | tFilterBlock | tObjectiveBlock | tConstraintBlock;
+
+    check_input_block<ComponentType::kFilter>(tCombinedInput, tFilterBlock, TEST_CONTEXT("Filterblock"));
+    check_input_block<ComponentType::kObjective>(tCombinedInput, tObjectiveBlock, TEST_CONTEXT("Objective block"));
+    check_input_block<ComponentType::kConstraint>(tCombinedInput, tConstraintBlock, TEST_CONTEXT("Constraint block"));
+
+    EXPECT_TRUE(tCombinedInput.get<ComponentType::kGeometry>().empty());
+    EXPECT_TRUE(tCombinedInput.get<ComponentType::kProcessManager>().empty());
+}
+
 }  // namespace plato::input_parser::unittest
