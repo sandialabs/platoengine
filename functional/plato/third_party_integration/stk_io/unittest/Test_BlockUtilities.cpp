eee50a53f673f116426629fd97d9c0221c21a03a
Brett Clark <bwclark@sandia.gov>
2025-04-28 11:23:21 -0600

    Updating unit tests to accommodate for changed mesh meta data ordinal in new version of STK

--- functional/plato/third_party_integration/stk_io/unittest/Test_BlockUtilities.cpp
+++ functional/plato/third_party_integration/stk_io/unittest/Test_BlockUtilities.cpp
@@ -34,16 +34,17 @@ TEST_F(TwoBlockMeshOnDisk, NumberOfBlocks)
 TEST_F(TwoBlockMeshOnDisk, BlockData)
 {
     const auto tBulkData = read_mesh_bulk_data(mMeshFilePath);
+    std::cout << mMeshFilePath.string() << std::endl;
 
     const auto tBlockIDsAndNames = block_data(*tBulkData);
 
     ASSERT_EQ(tBlockIDsAndNames.size(), mExpectedNumberOfBlocks);
 
     EXPECT_EQ(tBlockIDsAndNames.front().mID, 1);
-    EXPECT_EQ(tBlockIDsAndNames.front().mMetaDataOrdinal, 40u);
+    EXPECT_EQ(tBlockIDsAndNames.front().mMetaDataOrdinal, 45u);
     EXPECT_EQ(tBlockIDsAndNames.front().mName, "block_1");
     EXPECT_EQ(tBlockIDsAndNames.back().mID, 2);
-    EXPECT_EQ(tBlockIDsAndNames.back().mMetaDataOrdinal, 41u);
+    EXPECT_EQ(tBlockIDsAndNames.back().mMetaDataOrdinal, 46u);
     EXPECT_EQ(tBlockIDsAndNames.back().mName, "block_2");
 
     // Check sorted post-condition
@@ -57,13 +58,13 @@ TEST_F(TwoBlockMeshOnDisk, PartWithBlockNameAndID)
     const auto tTwoBlockMesh = read_mesh_bulk_data(mMeshFilePath);
     const auto tBlockData = block_data(*tTwoBlockMesh);
     {
-        const auto tBlock = part_with_block_meta_data_ordinal(*tTwoBlockMesh, 40u);
+        const auto tBlock = part_with_block_meta_data_ordinal(*tTwoBlockMesh, 45u);
         ASSERT_TRUE(tBlock);
         EXPECT_EQ(element_size(*tTwoBlockMesh, tBlock->get()), kExpectedNumberOfElementsInBlock1);
         EXPECT_EQ(node_size(*tTwoBlockMesh, tBlock->get()), kExpectedNumberOfNodesInBlock1);
     }
     {
-        const auto tBlock = part_with_block_meta_data_ordinal(*tTwoBlockMesh, 41u);
+        const auto tBlock = part_with_block_meta_data_ordinal(*tTwoBlockMesh, 46u);
         ASSERT_TRUE(tBlock);
         EXPECT_EQ(element_size(*tTwoBlockMesh, tBlock->get()), kExpectedNumberOfElementsInBlock2);
         EXPECT_EQ(node_size(*tTwoBlockMesh, tBlock->get()), kExpectedNumberOfNodesInBlock2);
@@ -167,7 +168,7 @@ TEST_F(OneBlock3x1x1HexMeshWithNodeSets, BlockData)
     ASSERT_EQ(tBlockData.size(), tNumberOfExpectedBlocks);
 
     EXPECT_EQ(tBlockData.front().mID, 1);
-    EXPECT_EQ(tBlockData.front().mMetaDataOrdinal, 40U);
+    EXPECT_EQ(tBlockData.front().mMetaDataOrdinal, 45U);
     EXPECT_EQ(tBlockData.front().mName, "block_1");
 }
 
