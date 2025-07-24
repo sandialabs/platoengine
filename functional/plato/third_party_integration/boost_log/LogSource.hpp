f8624799d3d23b1a15a496f584e22df4c53a51e7
Ray Wildman <rawildm@sandia.gov>
2025-07-24 13:58:43 -0600

    Removes LogSourceLoggerSink and adds a LogSource filter

--- functional/plato/third_party_integration/boost_log/LogSource.hpp
+++ functional/plato/third_party_integration/boost_log/LogSource.hpp
@@ -1,25 +1,30 @@
 #ifndef PLATO_THIRD_PARTY_INTEGRATION_BOOST_LOG_LOGSOURCE
 #define PLATO_THIRD_PARTY_INTEGRATION_BOOST_LOG_LOGSOURCE
 
+#include <boost/log/expressions/filter.hpp>
 #include <boost/log/expressions/keyword.hpp>
 #include <ostream>
 
 namespace plato::third_party_integration::boost_log
 {
+/// @brief Enum tag to distinguish log messages generated internally to platoengine vs. externally in shared libraries.
 enum struct LogSource
 {
     kInternal,
     kExternal
 };
 
-auto operator<<(std::ostream& aStream, LogSource aLogSource) -> std::ostream&;
+/// @brief Returns a filter that filters out messages not matching @a aLogSourceToInclude.
+[[nodiscard]] auto log_source_filter(LogSource aLogSourceToInclude) -> boost::log::filter;
 
-}  // namespace plato::third_party_integration::boost_log
+auto operator<<(std::ostream& aStream, LogSource aLogSource) -> std::ostream&;
 
 constexpr inline auto kLogSourceAttributeName = std::string_view{"Log source"};
 
+}  // namespace plato::third_party_integration::boost_log
+
 BOOST_LOG_ATTRIBUTE_KEYWORD(log_source,
-                            kLogSourceAttributeName.data(),
+                            plato::third_party_integration::boost_log::kLogSourceAttributeName.data(),
                             plato::third_party_integration::boost_log::LogSource)
 
 #endif
--- functional/plato/third_party_integration/boost_log/unittest/Test_LogSource.cpp
+++ functional/plato/third_party_integration/boost_log/unittest/Test_LogSource.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 
+#include <boost/log/attributes/constant.hpp>
 #include <sstream>
 
 #include "plato/test_utilities/TestContext.hpp"
@@ -20,4 +21,17 @@ TEST(LogSource, StreamInsertion)
     tExpectStreamMatches(LogSource::kInternal, "internal", TEST_CONTEXT("Internal"));
     tExpectStreamMatches(LogSource::kExternal, "external", TEST_CONTEXT("External"));
 }
+
+TEST(LogSource, LogSourceFilter)
+{
+    const auto tLogSourceFilter = log_source_filter(LogSource::kInternal);
+
+    auto tAttributeSet = boost::log::attribute_set{};
+    tAttributeSet.insert(boost::log::attribute_name{kLogSourceAttributeName.data()},
+                         boost::log::attributes::constant<LogSource>(LogSource::kInternal));
+
+    EXPECT_TRUE(tLogSourceFilter(
+        boost::log::attribute_value_set{tAttributeSet, boost::log::attribute_set{}, boost::log::attribute_set{}}));
+}
+
 }  // namespace plato::third_party_integration::boost_log::unittest
