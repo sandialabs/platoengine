45ce7973879f09fad98544a67b72be25ec220ced
Ray Wildman <rawildm@sandia.gov>
2025-07-25 12:20:52 -0600

    Adds an internal console sink for the main console logger

---
+++ functional/plato/third_party_integration/boost_log/InternalLoggerConsoleSink.hpp
@@ -0,0 +1,13 @@
+#ifndef PLATO_THIRD_PARTY_INTEGRATION_BOOST_LOG_INTERNALLOGGERCONSOLESINK
+#define PLATO_THIRD_PARTY_INTEGRATION_BOOST_LOG_INTERNALLOGGERCONSOLESINK
+
+#include "plato/third_party_integration/boost_log/LoggerSinkSetupTeardown.hpp"
+
+namespace plato::third_party_integration::boost_log
+{
+/// @brief Returns a sink that streams to the console via `std::cout`.
+/// @note This sink is filtered on the tag `LoggerSource::kInternal` and mpi rank 0.
+[[nodiscard]] auto internal_console_sink() -> LoggerSinkSetupTeardown;
+}  // namespace plato::third_party_integration::boost_log
+
+#endif
