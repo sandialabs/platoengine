2d7148f7c011274dae8f8f8b316073131e9e7970
Adam Christopher Sokolow <acsokol@sandia.gov>
2025-08-04 20:56:00 +0000

    Removes Proxy apps

--- apps/rosenbrock/LinearOperationMain.cpp
+++
@@ -1,9 +0,0 @@
-#include "Plato_LinearOperationApp.hpp"
-#include "Plato_TemplateMain.hpp"
-
-/******************************************************************************/
-int main(int aArgc, char *aArgv[])
-/******************************************************************************/
-{
-    Plato::Main<Plato::LinearOperationApp>(aArgc, aArgv);
-}
