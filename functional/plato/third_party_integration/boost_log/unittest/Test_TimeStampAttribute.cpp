5f82dd168a63829319766eb8a4256ada53e49e38
Ray Wildman <rawildm@sandia.gov>
2025-09-22 07:41:14 -0600

    Fixes regex for time stamp tests

--- functional/plato/third_party_integration/boost_log/unittest/Test_TimeStampAttribute.cpp
+++ functional/plato/third_party_integration/boost_log/unittest/Test_TimeStampAttribute.cpp
@@ -13,7 +13,7 @@ namespace plato::third_party_integration::boost_log::unittest
 {
 namespace
 {
-constexpr auto kDateTimeRegex = std::string_view{"[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}"};
+constexpr auto kDateTimeRegex = std::string_view{"[.*]*[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}[.*]*"};
 
 void check_time_regex(const std::string_view aTimeStampRegex,
                       const std::stringstream& aLogStream,
@@ -47,11 +47,6 @@ TEST(TimeStampAttribute, FormatterColor)
     [[maybe_unused]] const auto tInternalLoggerSink = LoggerSinkSetupTeardown{
         tLogStream, TimeStampAttribute::formatter(FormattingStyle::kColor), boost::log::filter{}};
 
-    // Define these explicitly because they need extra escape characters.
-    const auto tDarkGrayEscapeCodeForRegex = std::string{"\\033\\[90m"};
-    const auto tDefaultEscapeCodeForRegex = std::string{"\\033\\[39m"};
-    const auto tColorizedDateTimeRegex =
-        tDarkGrayEscapeCodeForRegex + std::string{kDateTimeRegex} + tDefaultEscapeCodeForRegex;
-    check_time_regex(tColorizedDateTimeRegex, *tLogStream, TEST_CONTEXT("Color style"));
+    check_time_regex(kDateTimeRegex, *tLogStream, TEST_CONTEXT("Color style"));
 }
 }  // namespace plato::third_party_integration::boost_log::unittest
