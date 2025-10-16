b9117e2cb21bcc6dcabd1c31b0984c40c75423fd
Ray Wildman <rawildm@sandia.gov>
2025-10-16 13:25:48 -0600

    Adds fPIC where needed

--- functional/plato/cmake/create_plato_library.cmake
+++ functional/plato/cmake/create_plato_library.cmake
@@ -33,6 +33,7 @@ function(create_plato_library_impl LIBRARY_NAME DIRECTORIES TARGET_LINK_LIST LIB
 
     add_library(${LIBRARY_NAME} ${LIB_TYPE} ${LIB_SRCS} ${LIB_HDRS} )
     target_include_directories(${LIBRARY_NAME} ${EXPORT_TYPE} $<BUILD_INTERFACE:${FUNCTIONAL_BASE_DIR}>)
+    set_target_properties(${LIBRARY_NAME} PROPERTIES POSITION_INDEPENDENT_CODE TRUE)
 
     target_link_libraries(${LIBRARY_NAME} ${EXPORT_TYPE} CoverageInterface ${TARGET_LINK_LIST})
     if(BUILD_WITH_CLANG_TIDY)
