dd18f8188eeba5afb3abeed56cd3680c7b29cdf4
Ray Wildman <rawildm@sandia.gov>
2025-10-29 09:22:16 -0600

    Removes unused function

--- functional/plato/process_manager/extension/ConstraintCompositionUtility.cpp
+++ functional/plato/process_manager/extension/ConstraintCompositionUtility.cpp
@@ -4,18 +4,6 @@
 
 namespace plato::process_manager::extension
 {
-auto compose_geometry_with_constraint(
-    const criteria::library::Constraint<const analysis::AnalysisDomainMesh&>& aMeshConstraint,
-    const plato::geometry::library::FactoryTypes& aGeometry)
-    -> criteria::library::Constraint<const linear_algebra::DynamicVector<double>&>
-{
-    auto tComposition = compose(aMeshConstraint.mConstraintFunction, aGeometry.mCompute);
-
-    return criteria::library::Constraint<const linear_algebra::DynamicVector<double>&>{
-        aMeshConstraint.mName, std::move(tComposition), aMeshConstraint.mConstraintTarget, aMeshConstraint.mLinear,
-        aMeshConstraint.mConstraintType};
-}
-
 auto compose_geometry_with_vector_constraint(
     const criteria::library::VectorConstraint<const analysis::AnalysisDomainMesh&>& aMeshConstraint,
     const plato::geometry::library::FactoryTypes& aGeometry)
