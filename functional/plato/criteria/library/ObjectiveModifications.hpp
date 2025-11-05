c7c6ef3130a412dfdfe5ec2b9fd64a3dd0dbe2cc
ralberd <ralberd@sandia.gov>
2025-11-05 14:44:18 -0700

    inlines reciprocal_function()

--- functional/plato/criteria/library/ObjectiveModifications.hpp
+++ functional/plato/criteria/library/ObjectiveModifications.hpp
@@ -8,14 +8,14 @@
 namespace plato::criteria::library
 {
 /// @brief Returns a core::Function that reciprocates an objective (given f, returns 1/f)
-[[nodiscard]] auto reciprocal_function();
+[[nodiscard]] inline auto reciprocal_function();
 
 /// @brief If @a aObjectiveGoal is kMinimizeReciprocal, composes the core::Function @a aFunction with a function that
 /// returns the reciprocal. Else, returns @a aFunction.
 template <typename F>
 [[nodiscard]] auto make_reciprocal_criterion_function(F aFunction, const ObjectiveGoal& aObjectiveGoal);
 
-[[nodiscard]] auto reciprocal_function()
+[[nodiscard]] inline auto reciprocal_function()
 {
     return core::make_function_with_first_derivative([](const double aArg) { return 1.0 / aArg; },
                                                      [](const double aArg) { return -1.0 / aArg / aArg; });
