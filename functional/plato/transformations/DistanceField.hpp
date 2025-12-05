034b70832894c6f909ed66df21d5f06cff3e77f5
Ray Wildman <rawildm@sandia.gov>
2025-12-05 12:21:56 -0700

    Adds some more tests and documentation

--- functional/plato/transformations/DistanceField.hpp
+++ functional/plato/transformations/DistanceField.hpp
@@ -5,23 +5,27 @@
 
 namespace plato::transformations
 {
-/// @brief Representation of a plane using a normal and an offset from the origin.
+/// @brief Representation of a plane using a normal and an offset from the origin (Hessian normal form).
+/// @note The sign of `mOriginSignedDistance` is positive if the half space formed by the plane and normal contains the
+/// origin, and it is negative otherwise.
 struct Plane
 {
-    double mOriginDistance = 0.0;
+    double mOriginSignedDistance = 0.0;
     third_party_integration::common::Vector3 mNormal;
 };
 
-/// @brief Computes the disnance from a point @a aPoint to a plane @a aPlane.
-[[nodiscard]] constexpr auto point_plane_distance(const Plane aPlane, const third_party_integration::common::Coordinate& aPoint) -> double;
+/// @brief Computes the distance from a point @a aPoint to a plane @a aPlane.
+[[nodiscard]] constexpr auto point_plane_signed_distance(
+    const Plane aPlane, const third_party_integration::common::Coordinate& aPoint) -> double;
 
-constexpr auto point_plane_distance(const Plane aPlane, const third_party_integration::common::Coordinate& aPoint) -> double
+constexpr auto point_plane_signed_distance(const Plane aPlane,
+                                           const third_party_integration::common::Coordinate& aPoint) -> double
 {
     namespace tpic = third_party_integration::common;
     constexpr auto tOrigin = tpic::Coordinate{0.0, 0.0, 0.0};
-    return tpic::dot(aPlane.mNormal, aPoint - tOrigin) / tpic::magnitude(aPlane.mNormal) + aPlane.mOriginDistance;
+    return tpic::dot(aPlane.mNormal, aPoint - tOrigin) / tpic::magnitude(aPlane.mNormal) + aPlane.mOriginSignedDistance;
 }
 
-}
+}  // namespace plato::transformations
 
 #endif
--- functional/plato/transformations/unittest/Test_DistanceField.cpp
+++ functional/plato/transformations/unittest/Test_DistanceField.cpp
@@ -7,44 +7,70 @@ namespace plato::transformations::unittest
 namespace
 {
 namespace tpic = third_party_integration::common;
-constexpr auto kTestPoint = tpic::Coordinate{.x=10.0, .y=11.0, .z=12.0};
-}
+constexpr auto kTestPoint = tpic::Coordinate{.x = 10.0, .y = 11.0, .z = 12.0};
+}  // namespace
 
 TEST(DistanceField, PointPlaneDistanceZeroOffsetCartesianNormals)
 {
-    const auto tXPlane = Plane{.mOriginDistance = 0.0, .mNormal = {.x = 1.0, .y = 0.0, .z = 0.0}};
-    EXPECT_EQ(point_plane_distance(tXPlane, kTestPoint), kTestPoint.x);
-
-    const auto tYPlane = Plane{.mOriginDistance = 0.0, .mNormal = {.x = 0.0, .y = 1.0, .z = 0.0}};
-    EXPECT_EQ(point_plane_distance(tYPlane, kTestPoint), kTestPoint.y);
-
-    const auto tZPlane = Plane{.mOriginDistance = 0.0, .mNormal = {.x = 0.0, .y = 0.0, .z = 1.0}};
-    EXPECT_EQ(point_plane_distance(tZPlane, kTestPoint), kTestPoint.z);
+    const auto tXPlane = Plane{.mOriginSignedDistance = 0.0, .mNormal = {.x = 1.0, .y = 0.0, .z = 0.0}};
+    const auto tYPlane = Plane{.mOriginSignedDistance = 0.0, .mNormal = {.x = 0.0, .y = 1.0, .z = 0.0}};
+    const auto tZPlane = Plane{.mOriginSignedDistance = 0.0, .mNormal = {.x = 0.0, .y = 0.0, .z = 1.0}};
+    {
+        EXPECT_EQ(point_plane_signed_distance(tXPlane, kTestPoint), kTestPoint.x);
+        EXPECT_EQ(point_plane_signed_distance(tYPlane, kTestPoint), kTestPoint.y);
+        EXPECT_EQ(point_plane_signed_distance(tZPlane, kTestPoint), kTestPoint.z);
+    }
+    {
+        constexpr auto tTestPointBelow = tpic::Coordinate{.x = -10.0, .y = -11.0, .z = -12.0};
+        EXPECT_EQ(point_plane_signed_distance(tXPlane, tTestPointBelow), tTestPointBelow.x);
+        EXPECT_EQ(point_plane_signed_distance(tYPlane, tTestPointBelow), tTestPointBelow.y);
+        EXPECT_EQ(point_plane_signed_distance(tZPlane, tTestPointBelow), tTestPointBelow.z);
+    }
 }
 
 TEST(DistanceField, PointPlaneDistanceNonZeroOffsetCartesianNormals)
 {
-    constexpr auto tOriginDistance = 5.0;
-    constexpr auto tXPlane = Plane{.mOriginDistance = tOriginDistance, .mNormal = {.x = 1.0, .y = 0.0, .z = 0.0}};
-    EXPECT_EQ(point_plane_distance(tXPlane, kTestPoint), kTestPoint.x + tOriginDistance);
+    constexpr auto tOriginDistance = -5.0;
+    constexpr auto tXPlane = Plane{.mOriginSignedDistance = tOriginDistance, .mNormal = {.x = 1.0, .y = 0.0, .z = 0.0}};
+    constexpr auto tYPlane = Plane{.mOriginSignedDistance = tOriginDistance, .mNormal = {.x = 0.0, .y = 1.0, .z = 0.0}};
+    constexpr auto tZPlane = Plane{.mOriginSignedDistance = tOriginDistance, .mNormal = {.x = 0.0, .y = 0.0, .z = 1.0}};
 
-    constexpr  auto tYPlane = Plane{.mOriginDistance = tOriginDistance, .mNormal = {.x = 0.0, .y = 1.0, .z = 0.0}};
-    EXPECT_EQ(point_plane_distance(tYPlane, kTestPoint), kTestPoint.y + tOriginDistance);
+    {
+        EXPECT_EQ(point_plane_signed_distance(tXPlane, kTestPoint), kTestPoint.x + tOriginDistance);
+        EXPECT_EQ(point_plane_signed_distance(tYPlane, kTestPoint), kTestPoint.y + tOriginDistance);
+        EXPECT_EQ(point_plane_signed_distance(tZPlane, kTestPoint), kTestPoint.z + tOriginDistance);
 
-    constexpr  auto tZPlane = Plane{.mOriginDistance = tOriginDistance, .mNormal = {.x = 0.0, .y = 0.0, .z = 1.0}};
-    EXPECT_EQ(point_plane_distance(tZPlane, kTestPoint), kTestPoint.z + tOriginDistance);
+        // Point on plane
+        constexpr auto tPointOnXPlane = tpic::Coordinate{.x = 5.0, .y = 10.0, .z = -3.0};
+        EXPECT_EQ(point_plane_signed_distance(tXPlane, tPointOnXPlane), 0.0);
+    }
+    {
+        constexpr auto tTestPointBelow = tpic::Coordinate{.x = -10.0, .y = -11.0, .z = -12.0};
+        EXPECT_EQ(point_plane_signed_distance(tXPlane, tTestPointBelow), tTestPointBelow.x + tOriginDistance);
+        EXPECT_EQ(point_plane_signed_distance(tYPlane, tTestPointBelow), tTestPointBelow.y + tOriginDistance);
+        EXPECT_EQ(point_plane_signed_distance(tZPlane, tTestPointBelow), tTestPointBelow.z + tOriginDistance);
+
+        // Point on plane
+        constexpr auto tPointOnYPlane = tpic::Coordinate{.x = 15.0, .y = 5.0, .z = -3.0};
+        EXPECT_EQ(point_plane_signed_distance(tYPlane, tPointOnYPlane), 0.0);
+    }
 }
 
 TEST(DistanceField, PointPlaneDistanceArbitrary)
 {
-    constexpr auto tOriginDistance = -30.0;
-    constexpr auto tNormal = tpic::Vector3{.x = 1.0, .y = -1.0, .z = 2.0};
-    constexpr auto tPlane = Plane{.mOriginDistance = tOriginDistance, .mNormal = tNormal};
+    constexpr auto tOriginDistance = 30.0;
+    constexpr auto tNormal = tpic::Vector3{.x = 3.0, .y = 0.0, .z = -4.0};
+    constexpr auto tNormalMagnitude = 5.0;
+    constexpr auto tPlane = Plane{.mOriginSignedDistance = tOriginDistance, .mNormal = tNormal};
     constexpr auto tOrigin = tpic::Coordinate{.x = 0.0, .y = 0.0, .z = 0.0};
-    const auto tExpected = tpic::dot(tNormal, kTestPoint - tOrigin) / magnitude(tNormal) + tOriginDistance;
-    EXPECT_EQ(point_plane_distance(tPlane, kTestPoint), tExpected);
+    constexpr auto tExpected = tpic::dot(tNormal, kTestPoint - tOrigin) / tNormalMagnitude + tOriginDistance;
+    EXPECT_DOUBLE_EQ(point_plane_signed_distance(tPlane, kTestPoint), tExpected);
 
     // Distance to origin
-    EXPECT_EQ(point_plane_distance(tPlane, tOrigin), tOriginDistance);
+    EXPECT_DOUBLE_EQ(point_plane_signed_distance(tPlane, tOrigin), tOriginDistance);
+
+    // Point on plane
+    constexpr auto tPointOnPlane = tpic::Coordinate{0.0, 0.0, -tOriginDistance * tNormalMagnitude / tNormal.z};
+    EXPECT_DOUBLE_EQ(point_plane_signed_distance(tPlane, tPointOnPlane), 0.0);
 }
-}
+}  // namespace plato::transformations::unittest
