385599fc0006ca7f1c5439427bb6e7ae54832526
Ray Wildman <rawildm@sandia.gov>
2026-01-13 13:31:06 -0700

    Adds checks for header include style

--- functional/plato/cmake/generate_precompiled_headers.cmake
+++ functional/plato/cmake/generate_precompiled_headers.cmake
@@ -14,8 +14,20 @@ function(precompiled_header_list ALL_FILES OUT_precompiled_header_list)
                 REGEX_LINE
                 "${INCLUDE_LINE}"
             )
-            list(APPEND RAW_HEADER_LIST "<${REGEX_LINE}$<ANGLE-R>")
+            if("${REGEX_LINE}" MATCHES "^plato\/.*")
+                message(SEND_ERROR "Found an internal plato header included with angle-brackets.\nHeader: ${REGEX_LINE}\nFile: ${SRC}")
+            else()
+                list(APPEND RAW_HEADER_LIST "<${REGEX_LINE}$<ANGLE-R>")
+            endif()
         endforeach()
+
+        file(STRINGS "${SRC}" MATCHED_INCLUDE_LINES REGEX "#include[ \t]*\"[^>]+\"" )
+        foreach(INCLUDE_LINE IN LISTS MATCHED_INCLUDE_LINES)
+            if(NOT "${INCLUDE_LINE}" MATCHES "#include[ \t]*\"plato\/.*" )
+                message(SEND_ERROR "Found an apparently external header included with quotes.\nHeader: ${INCLUDE_LINE}\nFile: ${SRC}")
+            endif()
+        endforeach()
+
     endforeach()
 
     list(REMOVE_DUPLICATES RAW_HEADER_LIST)
